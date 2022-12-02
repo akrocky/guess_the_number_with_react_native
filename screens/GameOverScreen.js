1f1774e280dfe2d4dc673df21fcd818443e5174f
akrocky <abul64604@gmail.com>
2022-12-02 10:21:33 +0100

    02122022--1021

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,14 +1,29 @@
-import { Dimensions, Image, StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, useWindowDimensions, View } from 'react-native';
 
 import PrimaryButton from '../components/ui/PrimaryButton';
 import Title from '../components/ui/Title';
 import Colors from '../constants/colors';
 
 function GameOverScreen({ roundsNumber, userNumber, onStartNewGame }) {
+  const {width,height}=useWindowDimensions();
+  let imageSize=300;
+  if (width<300) {
+    imageSize=150;
+  }
+  if (height<400) {
+    imageSize=80
+    
+  }
+  const imageStyle={
+    width:imageSize,
+    height:imageSize,
+    borderRadius:imageSize/2
+  }
   return (
+    <ScrollView style={styles.screen}>
     <View style={styles.rootContainer}>
       <Title>GAME OVER!</Title>
-      <View style={styles.imageContainer}>
+      <View style={[styles.imageContainer,imageStyle]}>
         <Image
           style={styles.image}
           source={require('../assets/images/success.png')}
@@ -21,12 +36,16 @@ function GameOverScreen({ roundsNumber, userNumber, onStartNewGame }) {
       </Text>
       <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
     </View>
+    </ScrollView>
   );
 }
 
 export default GameOverScreen;
-const deviceWidth=Dimensions.get('window').width;
+// const deviceWidth=Dimensions.get('window').width;
 const styles = StyleSheet.create({
+  screen:{
+    flex:1
+  },
   rootContainer: {
     flex: 1,
     padding: 24,
@@ -34,9 +53,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   imageContainer: {
-    width: deviceWidth<380?150:300,
-    height:deviceWidth<380?150:300,
-    borderRadius:deviceWidth<380?75:150,
+    // width: deviceWidth<380?150:300,
+    // height:deviceWidth<380?150:300,
+    // borderRadius:deviceWidth<380?75:150,
     borderWidth: 3,
     borderColor: Colors.primary800,
     overflow: 'hidden',
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,6 @@
 import { Ionicons } from '@expo/vector-icons';
 import { useEffect, useState } from 'react';
-import { Alert, FlatList, StyleSheet, View } from 'react-native';
+import { Alert, FlatList, StyleSheet, useWindowDimensions, View } from 'react-native';
 import GuessLogItem from '../components/Game/GuessLogItem';
 import NumberContainer from '../components/Game/NumberContainer';
 import Card from '../components/ui/Card';
@@ -31,6 +31,7 @@ function GameScreen({ userNumber, onGameOver }) {
       onGameOver(guessRounds.length);
     }
   }, [currentGuess, userNumber, onGameOver]);
+  const {width,height}=useWindowDimensions();
 
   useEffect(() => {
     minBoundary = 1;
@@ -65,11 +66,8 @@ function GameScreen({ userNumber, onGameOver }) {
   }
 
   const guessRoundsListLength = guessRounds.length;
-
-  return (
-    <View style={styles.screen}>
-      <Title>Opponent's Guess</Title>
-      <NumberContainer>{currentGuess}</NumberContainer>
+  let content=<>
+ <NumberContainer>{currentGuess}</NumberContainer>
       <Card>
         <InstructionText style={styles.instructionText}>
           Higher or lower?
@@ -86,7 +84,34 @@ function GameScreen({ userNumber, onGameOver }) {
             </PrimaryButton>
           </View>
         </View>
-      </Card>
+      </Card> 
+  </>
+  if (width>500) {
+    content=<>
+    <InstructionText style={styles.instructionText}>
+          Higher or lower?
+        </InstructionText>
+        <View style={styles.buttonsContainerWidth}>
+        <View style={styles.buttonContainer}>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>
+              <Ionicons name="md-remove" size={24} color="white" />
+            </PrimaryButton>
+          </View>
+          <NumberContainer>{currentGuess}</NumberContainer>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>
+              <Ionicons name="md-add" size={24} color="white" />
+            </PrimaryButton>
+          </View>
+        </View>
+   
+    </>
+  }
+
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+      {content}
       <View style={styles.listContainer}>
         {/* {guessRounds.map(guessRound => <Text key={guessRound}>{guessRound}</Text>)} */}
         <FlatList
@@ -120,6 +145,10 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flex: 1,
+  },
+  buttonsContainerWidth:{
+flexDirection:'row',
+alignItems:'center'
   },
   listContainer: {
     flex: 1,
